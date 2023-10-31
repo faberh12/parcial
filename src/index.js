7d3fc5ce9e77e58121b190b77e7038db86206e0b
fabian <hernandezfabian828@gmail.com>
2023-10-31 02:14:34 -0500

    "second commit"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import app from './app.js'
+import app from "./app.js";
+import { PORT } from "./config.js";
+import { connectDB } from "./db.js";
 
-app.listen(3000)
-console.log('server on port', 3000)
+async function main() {
+  try {
+    await connectDB();
+    app.listen(PORT);
+    console.log(`Listening on port http://localhost:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
